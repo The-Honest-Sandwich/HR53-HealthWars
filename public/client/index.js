d3da10c867e2ec3aa01647090109c78a58f83771
Patrick Sheehan <psheehan89@gmail.com>
2017-01-26 12:05:52 -0800

    Implemented App.js to route from index.js Currently doesn't render any components, but the connectivity is there.

--- public/client/index.js
+++ public/client/index.js
@@ -1,1 +1,8 @@
-console.log('We are index.js');
+//Client entry point
+import React from 'react';
+import { render } from 'react-dom';
+import {Router, Route, IndexRoute, hashHistory} from 'react-router';
+import App from './components/App';
+  
+render(
+  <App />, document.getElementById('app'));
