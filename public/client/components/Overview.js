3191f0ee21ce90c18ccd2c66a49390bb72a71657
James Briggs <jfbriggs106@gmail.com>
2017-01-30 15:47:50 -0800

    Edit Overview component to render UserTotal for set of data

--- public/client/components/Overview.js
+++ public/client/components/Overview.js
@@ -1,16 +1,34 @@
 import React from 'react';
+import UserTotal from './UserTotal.js';
+
+const data = [
+  {name: 'John Smith', team: 'Monkeys', total: 22},
+  {name: 'Alex Smith', team: 'Monkeys', total: 15},
+  {name: 'Greg Jones', team: 'Salamanders', total: 12},
+  {name: 'Ben Hamm', team: 'Salamanders', total: 13},
+  {name: 'George Forrest', team: 'Sharks', total: 17},
+  {name: 'Violet Jones', team: 'Creatures', total: 13},
+  {name: 'James Johnson', team: 'Salamanders', total: 18}
+];
 
 export default class Overview extends React.Component {
   render() {
     return (
-      <table>
-        <tr>
-          <th>Name</th>
-          <th>Organization</th>
-          <th>Total</th>
-        </tr>
-        <UserTotal />
-      </table>
+      <div id='overview'>
+        <div class='overview-header'>
+          Organization Totals
+        </div>
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Team</th>
+            <th>Total</th>
+          </tr>
+          { data.map((person) =>
+          <UserTotal name={person.name} team={person.team} total={person.total} />
+          ) }
+        </table>
+      </div>
     )
   }
 }
