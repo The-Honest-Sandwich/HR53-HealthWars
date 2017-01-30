9ca2cd4517874a2f88bc58d86858407e1ecdff4d
James Briggs <jfbriggs106@gmail.com>
2017-01-30 15:21:41 -0800

    Add barebones UserTotal component

---
+++ public/client/components/UserTotal.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class UserTotal extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <tr>
+        <td>{{this.props.name}}</td>
+        <td>{{this.props.organization}}</td>
+        <td>{{this.props.total}}</td>
+      </tr>
+    )
+  }
+}
