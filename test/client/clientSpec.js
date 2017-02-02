35b77afe47e138567a0094b036252234d6e5f403
James Briggs <jfbriggs106@gmail.com>
2017-02-01 16:27:56 -0800

    Add testing dependencies, create base test files/folders

---
+++ test/client/clientSpec.js
@@ -0,0 +1,10 @@
+// Just a sample test to check that Karma runs
+
+describe('Test tests', function() {
+
+  it('one should equal 1', function() {
+    var one = 1;
+    expect(one).toEqual(1);
+  });
+
+});
