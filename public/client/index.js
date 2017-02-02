8a03f40d7568495f62e76275ef060a1f6324abda
Patrick Sheehan <psheehan89@gmail.com>
2017-02-02 13:36:12 -0800

    Implemented react router for RESTful endpoints and navigation Assigned views to router endpoints

--- public/client/index.js
+++ public/client/index.js
@@ -3,5 +3,16 @@ import React from 'react';
 import { render } from 'react-dom';
 import {Router, Route, IndexRoute, hashHistory} from 'react-router';
 import App from './components/App';
+import LoggingExercise from './components/mainViews/LoggingExercise';
+import UserView from './components/UserView';
+import Overview from './components/Overview';
   
-render( <App />, document.getElementById('app'));
+render((
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+      <Route path="/user" component={UserView}/>
+      <Route path="/overview" component={Overview}/>
+      <Route path="/exercise" component={LoggingExercise}/>
+    </Route>
+  </Router>
+), document.getElementById('app'))
