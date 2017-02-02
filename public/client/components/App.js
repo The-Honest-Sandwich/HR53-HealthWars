49a8be80572b026fe632f2fbd1912344e6760d58
GregoryProuty <g.prout009@gmail.com>
2017-02-02 12:11:38 -0800

    ready for pull

--- public/client/components/App.js
+++ public/client/components/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-var UserView = require('./UserView.js')
+import LoggingExercise from './mainViews/LoggingExercise';
+import Overview from './Overview.js';
+import NavigationBar from './Navbar';
+
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        <UserView />
+        Components will go here.
+        <NavigationBar />
+        <Overview />
       </div>
     )
   }
