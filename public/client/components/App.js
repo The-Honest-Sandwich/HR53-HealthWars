5b74579fc85607faf38293d3b8a659c365ea47da
Franklin Jeng <franklinjjeng@gmail.com>
2017-02-03 16:46:32 -0800

    Passing DB information to all components through app.js

--- public/client/components/App.js
+++ public/client/components/App.js
@@ -2,13 +2,46 @@ import React from 'react';
 import LoggingExercise from './exercise/LoggingExercise';
 import Overview from './overview/Overview.js';
 import NavigationBar from './Navbar';
+import axios from 'axios';
 
 export default class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      rounds: null,
+      users: null,
+      exercise: null,
+
+    }
+  }
+
+  componentDidMount () {
+    var context = this;
+    axios.get('/api/rounds').then(function(res) {
+      context.setState({rounds: res.data});
+    });
+    axios.get('/api/users').then(function(res) {
+      context.setState({users: res.data});
+    });
+    axios.get('/api/exercises').then(function(res) {
+      context.setState({exercise: res.data});
+    });
+
+  }
+
   render() {
+    var context = this;
+    var children = React.Children.map(this.props.children, function(child) {
+      return React.cloneElement(child, {
+        rounds: context.state.rounds,
+        users: context.state.users,
+        exercise: context.state.exercise
+      })
+    })
     return (
       <div>
         <NavigationBar />
-        {this.props.children}
+        {children}
       </div>
     )
   }
