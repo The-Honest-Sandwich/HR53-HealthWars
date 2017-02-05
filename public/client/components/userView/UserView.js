13f2cc474c440e3bdabe37b3ad814d3e35a3a529
Franklin Jeng <franklinjjeng@gmail.com>
2017-02-05 13:25:39 -0800

    (feat) working on setting up user profile to utilize DB

--- public/client/components/userView/UserView.js
+++ public/client/components/userView/UserView.js
@@ -6,11 +6,29 @@ import dummydata from './../dummydata/userViewData';
 	
 
 export default class UserView extends React.Component {
+  constructor(props) {
+    // console.log('props in const', props)
+    super(props);
+    // this.state = {
+    //   currentUser: {name: '', team: ''}
+    // };
+  }
+
+  componentDidMount () {
+    // var context = this;
+    // console.log('outside', this.state.currentUser);
+    // console.log('outside props', this.props.currentUser);
+    // this.setState({currentUser: context.props.currentUser}, function() {
+    //   console.log('currentUser', context.state.currentUser);
+    // });
+  }
+
+
 
   render() {
     return (
       <div id='UserView'>
-      	<UserDescription />
+      	<UserDescription user={this.props.currentUser}/>
       	{dummydata.stats.map( (curWeek, i) => {
 			return <Week key={i} weekInfo={curWeek} />
 		})}
