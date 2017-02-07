ef3e8710652ac20858692157181f96ed9239252b
Patrick Sheehan <psheehan89@gmail.com>
2017-02-06 16:02:16 -0800

    Minor changes on the user profile stars

--- public/client/components/userView/Star.js
+++ public/client/components/userView/Star.js
@@ -3,14 +3,7 @@ import React from 'react';
 export default class Star extends React.Component {
   render() {
     return (
-      <svg className="star" height="40" width="50" viewBox="0 0 500 210">
-        <polygon points="100,10 40,198 190,78 10,78 160,198"
-        style={{fill:'red',
-                stroke:'red',
-                strokeWidth:5,
-                fillRule:'nonzero',
-              }} />
-      </svg>
+      <span className="glyphicon glyphicon-star" />
     )
   }
 }
